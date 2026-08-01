030a01158a132132e864c51e559e24527d384957
NOOBie_chan <odufuaomoghie9@gmail.com>
2026-08-01 13:35:09 +0100

    Added the Phosory Logo

--- public/settings.php
+++ public/settings.php
@@ -81,6 +81,7 @@ $theme = $my['theme'];
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>UniFile Settings</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+
 <style>
 :root{
     /* DARK THEME */
@@ -265,7 +266,7 @@ body.light .footer{ border-color: var(--border-light); color: var(--text-muted-l
 </div>
 
 <footer class="footer">
-    <a href="https://phosory.netlify.com" target="_blank" rel="noopener noreferrer">
+    <a href="https://phosory.netlify.com" target="_blank">
         <img src="icon.png" alt="Phosory Logo" class="footer-logo"/>
         Made by Phosory
     </a>
